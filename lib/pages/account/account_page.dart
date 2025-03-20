2494f49c5747c047fe426c3807ed9c9ad46ce27d
rishav8989 <betusenger@gmail.com>
2025-03-21 04:51:55 +0530

    logout button

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:inventory_management/main.dart';
 import 'package:inventory_management/pages/account/chat_page.dart';
 import 'package:inventory_management/pages/account/profile.dart';
 import 'package:inventory_management/pages/account/select_language.dart';
-import 'package:inventory_management/widgets/login/logout_button.dart';
+import 'package:inventory_management/pages/login_page.dart';
+import 'package:inventory_management/widgets/login/logout_confirmation_dialog.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
+
+  void _logout() async {
+    // Use LogoutConfirmationDialog to get confirmation
+    bool? confirmLogout =
+        await LogoutConfirmationDialog.show(); // No context needed
+
+    if (confirmLogout == true) {
+      pb.authStore.clear();
+      print('Logged out from AccountPage!');
+      Get.offAll(() => const LoginPage());
+    } else if (confirmLogout == false) {
+      print('Logout cancelled from AccountPage');
+      Get.snackbar(
+        // Use Get.snackbar for Snackbar
+        'Logout cancelled',
+        'Logout cancelled',
+        duration: const Duration(seconds: 2),
+      );
+    } else {
+      print('Logout dialog dismissed without choice (AccountPage)');
+    }
+  }
 
   Future<void> _launchWebsite() async {
     final Uri url = Uri.parse('https://rishavwiki.netlify.app/');
@@ -49,7 +73,9 @@ class AccountPage extends StatelessWidget {
                       children: <Widget>[
                         const CircleAvatar(
                           radius: 40,
-                          backgroundImage: NetworkImage('https://rishavwiki.netlify.app/assets/1707189968207-01.jpeg'),
+                          backgroundImage: NetworkImage(
+                            'https://rishavwiki.netlify.app/assets/1707189968207-01.jpeg',
+                          ),
                         ),
                         const SizedBox(width: 20),
                         Column(
@@ -61,7 +87,10 @@ class AccountPage extends StatelessWidget {
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20,
-                                color: Theme.of(context).textTheme.bodyMedium?.color,
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodyMedium?.color,
                               ),
                             ),
                             const SizedBox(height: 8),
@@ -81,19 +110,27 @@ class AccountPage extends StatelessWidget {
 
                 Card(
                   elevation: isDesktop ? 2 : 0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
                   child: Column(
                     children: [
-                      // _buildListTile(Icons.share, 'app_sharing'.tr, const ApplicationSharingPage()),
-                      _buildListTile(Icons.web, 'website'.tr, null, () => _launchWebsite()),
-                      // _buildListTile(Icons.share, 'app_sharing'.tr, const ApplicationSharingPage()),
-                      // _buildListTile(Icons.description, 'declaration'.tr, const DeclarationPage()),
-                      // _buildListTile(Icons.info_outline, 'about'.tr, const AboutPage()),
-
-                      // // New Tabs - Navigating to Pages
-                      _buildListTile(Icons.chat_bubble, 'chat_with_us'.tr, const ChatPage()),
-                      _buildListTile(Icons.language, 'language'.tr, const SelectLanguage()),
-                      // _buildListTile(Icons.color_lens, 'theme_selector'.tr, const ThemeSettingsPage()),
+                      _buildListTile(
+                        Icons.web,
+                        'website'.tr,
+                        null,
+                        () => _launchWebsite(),
+                      ),
+                      _buildListTile(
+                        Icons.chat_bubble,
+                        'chat_with_us'.tr,
+                        const ChatPage(),
+                      ),
+                      _buildListTile(
+                        Icons.language,
+                        'language'.tr,
+                        const SelectLanguage(),
+                      ),
                     ],
                   ),
                 ),
@@ -106,7 +143,10 @@ class AccountPage extends StatelessWidget {
                     child: SizedBox(
                       height: 50.0,
                       child: ElevatedButton(
-                        onPressed: () => LogoutButton,
+                        onPressed:
+                            () =>
+                                _logout(),
+
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.red,
                           foregroundColor: Colors.white,
@@ -124,18 +164,36 @@ class AccountPage extends StatelessWidget {
     );
   }
 
-  ListTile _buildListTile(IconData icon, String title, Widget? page, [VoidCallback? onTap]) {
+  ListTile _buildListTile(
+    IconData icon,
+    String title,
+    Widget? page, [
+    VoidCallback? onTap,
+  ]) {
     return ListTile(
       dense: true,
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 8.0,
+      ),
       leading: Icon(icon, color: Theme.of(Get.context!).iconTheme.color),
-      title: Text(title, style: TextStyle(color: Theme.of(Get.context!).textTheme.bodyMedium?.color)),
-      trailing: Icon(Icons.chevron_right, color: Theme.of(Get.context!).disabledColor),
-      onTap: onTap ?? () {
-        if (page != null) {
-          Get.to(() => page);
-        }
-      },
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(Get.context!).textTheme.bodyMedium?.color,
+        ),
+      ),
+      trailing: Icon(
+        Icons.chevron_right,
+        color: Theme.of(Get.context!).disabledColor,
+      ),
+      onTap:
+          onTap ??
+          () {
+            if (page != null) {
+              Get.to(() => page);
+            }
+          },
     );
   }
 }
